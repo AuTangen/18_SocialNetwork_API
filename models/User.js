1f1bbd2644e3b1a9406fdfbad7756c094a83a7c4
AuTangen <atangen2406@gmail.com>
2023-04-28 09:53:43 -0400

    initial, server/connection

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const  {Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema ({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(value) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+            },
+            message: "Please enter a valid email"
+    }
+}
+});
